23af41f1644572d98e03215bf151a578fa47a769
MD SAHIL HASNAIN <mdsahil1631@gmail.com>
2026-08-31 13:02:26 +0530

    commit

--- constants/config.ts
+++ constants/config.ts
@@ -1,3 +1,3 @@
 export const GITHUB_API = "https://api.github.com";
 
-export const GITHUB_CLIENT_ID = process.env.EXPO_PUBLIC_GITHUB_CLIENT_ID?.trim() ?? "";
+export const GITHUB_CLIENT_ID = process.env.EXPO_PUBLIC_GITHUB_CLIENT_ID?.trim() || "Ov23lill5gRD5i8esuW9";
